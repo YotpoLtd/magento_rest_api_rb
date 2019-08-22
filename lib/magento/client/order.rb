09ea39710bfe72cff6b64984e8beea59c30e7760
Doron Eshel <edoron@yotpo.com>
2019-08-22 16:18:24 +0300

    feat(Order):get order id by increment id

--- lib/magento/client/order.rb
+++ lib/magento/client/order.rb
@@ -8,6 +8,11 @@ module Magento
         get_wrapper("/V1/orders/#{order_id}", headers)
       end
 
+      def get_order_id_by_increment_id(increment_id)
+        headers = admin_headers
+        get_wrapper("/V1/orders?searchCriteria[filter_groups][2][filters][0][field]=increment_id&searchCriteria[filter_groups][2][filters][0][value]=#{increment_id}&searchCriteria[filter_groups][2][filters][0][condition_type]=eq&fields=items[items[order_id]]", headers)
+      end
+
       def invoice_order(order_id)
         headers = admin_headers
         post_wrapper("/V1/order/#{order_id}/invoice", {}.to_json, headers)
